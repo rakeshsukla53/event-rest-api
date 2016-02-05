1d7eb7f841cf84fdc4361c5e5784d29c25832b56
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-02-04 23:27:37 -0500

    event brite data collection identified

--- source/activites/management/commands/eventbrite_data.py
+++ source/activites/management/commands/eventbrite_data.py
@@ -7,9 +7,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         eventbrite = Eventbrite(generic_keys.eventbrite_oauth_token)
+
+        # for getting all the events from washington DC
         # washington_events = eventbrite.event_search(**{'venue.city': 'washington dc'})
         # print washington_events
 
+        # getting all the venue ID
+        # print eventbrite.get('/venues/12847605')
+
+        # getting all the ticket info
+        # print eventbrite.get_event_ticket_classes(event_id=12882727585)
         # passing parameters in url https://www.eventbriteapi.com/v3/events/search/?token=t&q=washington%20dc
 
 
